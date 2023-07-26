06d4b9bdbc28341528b5fe69a729debbceb713fb
hzw <huazhongwei@yeah.net>
2023-07-26 11:36:07 +0800

    页面完成

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,58 +19,336 @@
             <h5>PEOPLE SAID</h5>
         </div>
 
+        <div class="secondTextLayout">
+            <div class="oneLineLayout">
+                <img
+                    class="imageOne"
+                    referrerpolicy="no-referrer"
+                    src="../assets/image/ic_man.png"/>
+
+                <div class="oneTextLayout">
+                    <h1 class="oneTitle">Always Fresh, Always Fun</h1>
+                    <p class="oneContent">I’ve been playing Tapwinner for a while and have always enjoyed the games and the app! About a month ago there was a huge and awesome update to the app and I love all the changes! There is now a ranking system with cash paid out depending on rank, and a new ticket system that offers rewards in exchange for tickets.<br /><br />Stacy C Joined 05/02 2019</p>
+                </div>
+            </div>
+            <div class="oneLine"></div>
+            <div class="twoLineLayout">
+                <div class="twoTextLayout">
+                    <h1 class="twoTitle">Easy navigation, endless excitement</h1>
+                    <p class="twoContent">Completely addicted to this app! It is easy to navigate and the designers are always introducing new tasks and challenges. I, also, like how many ways I have to either win money or play for tickets. The new option of paying with tickets to get money was an awesome addition.<br /><br />Bagelslinger Joined 05/28 2019</p>
+                </div>
+                <img
+                    class="imageTwo"
+                    referrerpolicy="no-referrer"
+                    src="../assets/image/ic_woman.png"/>
+            </div>
+            <div class="twoLine"></div>
+            <div class="threeLineLayout">
+                <img
+                    class="imageThree"
+                    referrerpolicy="no-referrer"
+                    src="../assets/image/ic_old_man.png"/>
+
+                <div class="threeTextLayout">
+                    <h1 class="threeTitle">Real Rewards, Easy Entry</h1>
+                    <p class="threeContent">I'd say that Tap winner is one of the apps I use most on my phone. There's Instagram, Facebook (Marketplace especially), Tiktok, and Pocket7Games. Seriously, it's just like the games they play, except I can win money! Speaking of tournaments, that's probably my favorite and also a curse at the same time.  You'll get all the bonuses from making regular deposits and you'll make some nice little prizes every so often. Try it out and see!<br /><br />Chaerithecatty Joined 9/13/2021</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="thirdTextLayout">
+            <h1>Want to Play Games That Pay Real Money?</h1>
+            <p>Are you looking for money making games? Unlock your gaming potential with our selection of the best games to win real money! </p>
+            <p>​Don't waste your time on games that don't pay.  Look no further than casino apps that pay real money. With the rise of mobile technology, there are now many options for playing casino games right from your smartphone or tablet. But not all casino apps are fair. If you are interested in earning real money, it's important to choose money making games app that offers legitimate and secure options for cashing out your winnings.</p>
+            ​<p>One popular option for casino apps that pay real money is Tapwinner. Our app offers a range of games that are not only fun and addictive, but also offer the chance to win real money. From classic card games like blackjack and poker, to exciting new games like marble blast and 8 ball pool, and more.</p>
+            <p>So what are you waiting for? Start playing our legit money making games and get paid today!</p>
+        </div>
+
+        <div class="fourthTextLayout">
+            <h1>Legit Cash Games: Casino Apps That Pay Real Money</h1>
+            <p>To get started, simply download the app from your app store and create an account. You'll be able to start playing right away, and can even earn bonus cash just for signing up.</p>
+            <p>And when you're ready to cash out your winnings, tapwinner makes it easy to transfer your funds to your PayPal account or other preferred payment methods.</p>
+            <p>But tapwinner isn't the only casino app that pays real money. Many other options are available in both the app store and through your browser, so be sure to research and choose an app that is right for you.</p>
+            <p>And remember, when playing free casino games for real money, it's important to always play responsibly and never risk more than you can afford to lose.</p>
+            <p>Discover casino apps that match your interests and gain insider tips on how to win with us. You will be the next big winner! Explore now and  download free games today</p>
+        </div>
+
+        <img class="cashBattle" src="../assets/image/ic_cash_battle.png"/>
+    
+        <div class="lastLine"></div>
+
+        <div class="bottomLayout">
+            <div class="bottomOneLayout">
+                <div>About Us</div>
+                <div>Privacy Policy.</div>
+                <div>Terms of Service</div>
+            </div>
+
+            <div class="bottomTwoLayout">
+                <div>Contact Us</div>
+                <div>Player FAQ</div>
+            </div>
+
+            <div class="bottomThreeLayout">
+                <img src="../assets/image/ic_app_store.png"/>
+                <img src="../assets/image/ic_google_play.png"/>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <style scoped>
-.firstTextLayout {
+.bottomThreeLayout {
+    display: flex;
+    flex-flow: column;
+}
+
+.bottomThreeLayout img:nth-of-type(2) {
+    margin-top: 30px;
+}
+.bottomTwoLayout div {
+    font-size: 22px;
+    font-weight: 400;
+    color: #C9CACA;
+}
+.bottomTwoLayout div:nth-of-type(2) {
+    margin-top: 30px;
+}
+
+.bottomOneLayout div {
+    font-size: 22px;
+    font-weight: 400;
+    color: #C9CACA;
+}
+.bottomOneLayout div:nth-of-type(2) {
+    margin-top: 30px;
+}
+
+.bottomOneLayout div:nth-of-type(3) {
+    margin-top: 30px;
+}
+
+.bottomLayout {
+    display: flex;
+    width: 70%;
+    flex-flow: row;
+    justify-content: space-between;
     position: absolute;
-    top: 28%;
+    top: 94%;
+}
+
+.lastLine {
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    background-color: #FFFFFF;
+    top: 92.5%;
+}
+
+.cashBattle {
+    position: absolute;
+    top: 84%;
     left: 50%;
     transform: translate(-50%, -50%);
+    width: 946px;
+    height: 1126px;
+}
+
+.fourthTextLayout {
+    position: absolute;
+    top: 71%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+}
+
+.fourthTextLayout h1 {
+    font-size: 31px;
+    font-weight: 700;
+    color: #FFFFFF;
+    text-align: center;
+}
+
+.fourthTextLayout p {
+    font-size: 20px;
+    font-weight: 700;
+    color: #FFFFFF;
+    line-height: 1.5;
+    margin-top: 40px;
+}
+
+.thirdTextLayout {
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+}
+
+.thirdTextLayout h1 {
+    font-size: 36px;
+    font-weight: 700;
+    color: #FFFFFF;
+    text-align: center;
+}
+
+.thirdTextLayout p {
+    font-size: 20px;
+    font-weight: 700;
+    color: #FFFFFF;
+    line-height: 1.5;
+    margin-top: 40px;
+}
+
+.twoLine {
+    width: 100%;
+    height: 2px;
+    background-color: #C9CACA;
+    margin-top: 75px;
+}
+.imageTwo {
+    width: 298px;
+    height: 264px;
+}
+
+.threeLineLayout {
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    margin-top: 75px;
+}
+
+.twoLineLayout {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    margin-top: 75px;
+}
+
+.imageTwo {
+    width: 230px;
+    height: 246px;
+}
+
+.secondTextLayout {
+    position: absolute;
+    top: 44%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 70%;
+}
+
+.secondTextLayout h1 {
+    overflow-wrap: break-word;
+    color: #595757;
+    font-size: 30px;
+    font-family: Arial-Black;
+    font-weight: 700;
+    text-align: justifyLeft;
+    white-space: nowrap;
+    line-height: 30px;
+}
+.secondTextLayout p {
+    overflow-wrap: break-word;
+    color: #595757;
+    font-size: 22px;
+    font-family: NotoSansHans-Regular;
+    font-weight: 400;
+    text-align: justifyLeft;
+    line-height: 1.5;
+    margin-top: 44px;
+}
+
+.oneLine {
+    width: 100%;
+    height: 2px;
+    background-color: #C9CACA;
+    margin-top: 75px;
+}
+
+.oneTextLayout {
+    margin-left: 50px;
+}
+
+
+.oneLineLayout {
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+}
+
+.imageOne {
+    width: 252px;
+    height: 258px;
+}
+
+.firstTextLayout {
+    position: absolute;
+    top: 29%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
 }
 .firstTextLayout h3 {
     text-align: center;
     color: #711EA1 ;
     font-size: 30px;
+    font-weight: 600;
+    margin-top: 32px;
+}
+
+.firstTextLayout h3:nth-of-type(2) {
+    margin-top: 100px;
+}
+
+.firstTextLayout h5 {
+    text-align: center;
+    color: #711EA1 ;
+    font-size: 30px;
     font-weight: 400;
+    margin-top: 120px;
+    width: 100%;
 }
 
 .firstTextLayout p {
     font-size: 22px;
-    font-family: Noto Sans S Chinese;
     font-weight: 400;
     color: #595757;
-    width: 400px;
+    text-align: center;
+    margin: 24px 0;
+    width: 100%;
+    line-height: 1.5;
 }
 
 .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
     position: relative;
     width: 100%;
-    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 .topFirst {
-    width: 1920px;
+    width: 100%;
     height: 1383px;
     position: relative;
     background: url('../assets/image/bg_top.png');
+    background-repeat: round;
 }
 .topSecond {
-    width: 1920px;
+    width: 100%;
     height: 6800px;
     position: relative;
-    top: -207px;
+    margin-top: -207px;
     background: url('../assets/image/bg_second.png');
+    background-repeat: round;
 }
 
 .moneyIcon {
     position:absolute;
     top: 0;
     left: 50%;
+    width: 100%;
     transform: translate(-50%, 0);
 }
 .topWinner {
